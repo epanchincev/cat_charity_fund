b2607a98d596280da69d9fc3743fd1edab6c7576
Алексей Епанчинцев <alexey@epanchincev.ru>
2023-06-06 19:59:04 +0500

    rename default db

--- app/core/config.py
+++ app/core/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
 
     app_title: str = 'Кошачий благотворительный фонд'
     description: str = 'Сервис для поддержки котиков!'
-    database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
+    database_url: str = 'sqlite+aiosqlite:///./cat_charity_fund.db'
     secret: str = 'secretkeyasfas'
     first_superuser_email: Optional[EmailStr] = 'admin@admin.ru'
     first_superuser_password: Optional[str] = 'StrongPassword'
