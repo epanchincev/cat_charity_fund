e648f57e07a4f6cb396817ac163242259e6293a5
Алексей Епанчинцев <alexey@epanchincev.ru>
2023-06-06 11:47:59 +0500

    import flake8

--- app/core/base.py
+++ app/core/base.py
@@ -1,2 +1,2 @@
 from app.core.db import Base  # noqa
-from app.models import CharityProject, Donation, FinancialBase ,User  # noqa
+from app.models import CharityProject, Donation, FinancialBase, User  # noqa
