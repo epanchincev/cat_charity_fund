8c063a8ea45ce783b41b6a2c59e1d23ed537251b
Алексей Епанчинцев <alexey@epanchincev.ru>
2023-06-06 17:20:04 +0500

    del startup create user

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 
 from app.api.routers import main_router
 from app.core.config import settings
-from app.core.init_db import create_first_superuser
 
 app = FastAPI(
     title=settings.app_title,
@@ -15,4 +14,4 @@ app.include_router(main_router)
 @app.on_event('startup')
 async def startup() -> None:
     """Действия при запуске"""
-    await create_first_superuser()
+    pass
